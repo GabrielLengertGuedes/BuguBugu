b47ef136a6ce3ad2d9a768cec8ee3dae1219c36e
GabrielLengertGuedes <gabriellengguedes@gmail.com>
2026-09-02 00:42:11 -0300

    Atualiza site: classificação, jogos e notícias até a 5ª rodada
    - classificacao.json: tabela completa até a rodada 5 (Bugu Bugu em 3º, 12 pts)
    - jogos.json: resultados das rodadas 1 a 5 (2 vitórias por W.O.), descanso da
      rodada 3 restaurado, datas ajustadas, rodada 6 sem data, rodada 7 por W.O.
    - noticias.json: notícias sobre a derrota pro AGFF, as desistências de Audax e
      Onda Laranja, e a vitória sobre o FIFO
    - carregar-jogos.js: suporte a jogo por W.O., rodada sem data e placar de
      pênaltis vazio
    - componentes.css: estilo do placar de W.O.

--- js/carregar-jogos.js
+++ js/carregar-jogos.js
@@ -53,16 +53,26 @@ async function carregarJogos() {
   }
 }
 
-/* Monta o HTML de um jogo já realizado com placar e indicação de pênaltis. */
+/* Monta o HTML de um jogo já realizado com placar e indicação de pênaltis ou W.O. */
 function montarJogoRealizado(jogo) {
   var dt = formatarData(jogo.data, 'mes-dia');
   var buguVenceuPenaltis = jogo.vencedorPenaltis === 'BUGU BUGU';
 
-  // Placar com fundo dourado quando o Bugu Bugu ganhou os pênaltis (+1 ponto)
-  var classePlacar = buguVenceuPenaltis ? 'res pen' : 'res';
-  var tituloPlacar = buguVenceuPenaltis
-    ? 'Vitória nos pênaltis ' + jogo.placarPenaltis + ' (+1 ponto)'
-    : 'Derrota nos pênaltis';
+  // Jogo decidido por W.O. (adversário desistiu da liga): sem disputa de pênaltis
+  var classePlacar, tituloPlacar;
+  if (jogo.wo) {
+    classePlacar = 'res wo';
+    tituloPlacar = 'Resultado por W.O.';
+  } else if (jogo.vencedorPenaltis) {
+    // Placar com fundo dourado quando o Bugu Bugu ganhou os pênaltis (+1 ponto)
+    classePlacar = buguVenceuPenaltis ? 'res pen' : 'res';
+    tituloPlacar = buguVenceuPenaltis
+      ? 'Vitória nos pênaltis' + (jogo.placarPenaltis ? ' ' + jogo.placarPenaltis : '') + ' (+1 ponto)'
+      : 'Derrota nos pênaltis';
+  } else {
+    classePlacar = 'res';
+    tituloPlacar = 'Resultado no tempo normal';
+  }
 
   return [
     '<div class="jogo">',
@@ -71,6 +81,7 @@ function montarJogoRealizado(jogo) {
     '    <span class="nosso">Bugu Bugu</span>',
     '    <span class="vstag"> vs </span>',
     '    ' + capitalizarNome(jogo.adversario),
+    (jogo.wo ? '    <span class="vstag"> (W.O.)</span>' : ''),
     '  </div>',
     '  <div class="' + classePlacar + '" title="' + tituloPlacar + '">',
     '    ' + jogo.golsBuguBugu + '–' + jogo.golsAdversario,
@@ -86,27 +97,37 @@ function montarJogoFuturo(jogo) {
     return '<div class="jogo folga">' + dt.dia + ' ' + dt.mes + ' &middot; Rodada ' + jogo.rodada + ' &mdash; Descanso</div>';
   }
 
-  var dt = formatarData(jogo.data, 'mes-dia');
+  // Rodada ainda sem data marcada: mostra "Rodada N" no lugar do dia/mês
+  var temData = jogo.data && jogo.data.indexOf('-') !== -1;
+  var quando = temData
+    ? formatarData(jogo.data, 'mes-dia')
+    : { mes: 'Rodada&nbsp;', dia: jogo.rodada };
+
   return [
     '<div class="jogo">',
-    '  <div class="quando">' + dt.mes + '<b>' + dt.dia + '</b></div>',
+    '  <div class="quando">' + quando.mes + '<b>' + quando.dia + '</b></div>',
     '  <div class="conf">',
     '    <span class="nosso">Bugu Bugu</span>',
     '    <span class="vstag"> vs </span>',
     '    ' + capitalizarNome(jogo.adversario),
     '  </div>',
-    '  <span class="vstag">a jogar</span>',
+    '  <span class="vstag">' + (temData ? 'a jogar' : 'data a definir') + '</span>',
     '</div>'
   ].join('\n');
 }
 
 /* Monta o HTML interno da faixa dourada do próximo jogo na Home. */
 function montarFaixaProximoJogo(jogo) {
-  var dt = formatarData(jogo.data, 'mes-dia');
+  var temData = jogo.data && jogo.data.indexOf('-') !== -1;
+  var dataTexto = 'Data a definir';
+  if (temData) {
+    var dt = formatarData(jogo.data, 'mes-dia');
+    dataTexto = 'Sáb ' + dt.dia + ' ' + dt.mes;
+  }
   return [
     '<span class="fj-rotulo">&#9658; Próxima rodada</span>',
     '<span class="fj-confronto">Bugu Bugu &nbsp;&times;&nbsp; ' + capitalizarNome(jogo.adversario) + '</span>',
-    '<span class="fj-data">Sáb ' + dt.dia + ' ' + dt.mes + ' &middot; Rodada ' + jogo.rodada + '</span>'
+    '<span class="fj-data">' + dataTexto + ' &middot; Rodada ' + jogo.rodada + '</span>'
   ].join('\n');
 }
 
